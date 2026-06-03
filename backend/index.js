f455c17a44314b72db77752a545223f712747616
Divyansh <divyanshchoudhary789@gmail.com>
2026-06-03 13:58:57 +0530

    frontend url update

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "https://productr-rouge.vercel.app/",
         credentials: true,
     })
 );
